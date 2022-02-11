6498dbb0b92b54c510d8a118645693006ae69f7a
Luthaf <luthaf@luthaf.fr>
2022-02-11 12:15:58 +0100

    Compute a full array of Clebsch-Gordan coefficients in parallel

--- python/tests/wigner.py
+++ python/tests/wigner.py
@@ -1,6 +1,7 @@
 import math
 import unittest
 
+import numpy as np
 import wigners
 
 
@@ -29,3 +30,18 @@ class WignersTest(unittest.TestCase):
             wigners.clebsch_gordan(j1=1, m1=1, j2=1, m2=0, j3=2, m3=1),
             1 / math.sqrt(2.0),
         )
+
+    def test_clebsch_gordan_array(self):
+        j1 = 12
+        j2 = 15
+        j3 = 8
+
+        expected = np.zeros((2 * j1 + 1, 2 * j2 + 1, 2 * j3 + 1), dtype=np.float64)
+        for m1 in range(-j1, j1 + 1):
+            for m2 in range(-j2, j2 + 1):
+                for m3 in range(-j3, j3 + 1):
+                    expected[j1 + m1, j2 + m2, j3 + m3] = wigners.clebsch_gordan(
+                        j1, m1, j2, m2, j3, m3
+                    )
+
+        self.assertTrue(np.all(wigners.clebsch_gordan_array(j1, j2, j3) == expected))
--- python/wigners/__init__.py
+++ python/wigners/__init__.py
@@ -1,5 +1,6 @@
 import os
 import ctypes
+import numpy as np
 
 __lib = ctypes.CDLL(os.path.join(os.path.dirname(__file__), "_wigners.so"))
 
@@ -24,17 +25,56 @@ __lib.clebsch_gordan.argtypes = [
 __lib.clebsch_gordan.restype = ctypes.c_double
 
 
+__lib.clebsch_gordan_array_c.argtypes = [
+    ctypes.c_uint32,
+    ctypes.c_uint32,
+    ctypes.c_uint32,
+    ctypes.POINTER(ctypes.c_double),
+]
+__lib.clebsch_gordan_array_c.restype = None
+
+
 __lib.clear_wigner_3j_cache.argtypes = []
 __lib.clear_wigner_3j_cache.restype = None
 
 
 def wigner_3j(j1: int, j2: int, j3: int, m1: int, m2: int, m3: int) -> float:
+    """
+    Compute a single Wigner 3j coefficient, corresponding to:
+
+    .. code-block::
+
+        | j1  j2  j3 |
+        | m1  m2  m3 |
+    """
     return __lib.wigner_3j(j1, j2, j3, m1, m2, m3)
 
 
 def clebsch_gordan(j1: int, m1: int, j2: int, m2: int, j3: int, m3: int) -> float:
+    """
+    Compute a single Clebsch-Gordan coefficient, corresponding to:
+
+    .. code-block::
+
+        <j1 m1  j2 m2 | j3 m3>
+    """
     return __lib.clebsch_gordan(j1, m1, j2, m2, j3, m3)
 
 
+def clebsch_gordan_array(j1: int, j2: int, j3: int) -> np.ndarray:
+    """
+    Compute a full array of Clebsch-Gordan coefficient for the three given
+    ``j``.
+
+    The result is a 3-dimensional array with shape ``(2 * j1 + 1, 2 * j2 + 1, 2
+    * j3 + 1)``.
+    """
+    array = np.zeros((2 * j1 + 1, 2 * j2 + 1, 2 * j3 + 1), dtype=np.float64)
+    ptr = array.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
+    __lib.clebsch_gordan_array_c(j1, j2, j3, ptr, array.size)
+    return array
+
+
 def clear_wigner_3j_cache():
+    """Clear the LRU cache of Wigner 3j symbols"""
     return __lib.clear_wigner_3j_cache()
